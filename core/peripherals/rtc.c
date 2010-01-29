2944902f45605e8af1e03b227b46acf4e81f902e
nalim68 <nalim68@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-01-29 10:48:51 +0000

    dynawa.peek() fixed

--- core/peripherals/rtc.c
+++ core/peripherals/rtc.c
@@ -1,25 +1,124 @@
 #include "rtc.h"
 #include "i2c.h"
 
-int rtc_set() {
+#include "FreeRTOS.h"
+#include "hardware_conf.h"
+#include "debug/trace.h"
+#include <time.h>
+
+#define BCD2BIN(b) ((((b) & 0xf0) >> 4) * 10 + ((b) & 0x0f))
+#define BIN2BCD(b) ((((b) / 10) << 4) | ((b) % 10))
+
+int rtc_open() {
+    i2c_open();
+}
+
+int rtc_close() {
+    i2c_close();
+}
+
+int rtc_write(struct tm *new_time) {
+    TRACE_INFO("rtc_write()\r\n");
+
+    // rtc_wake();
+    portENTER_CRITICAL ();
+
     i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGSEC, I2CMASTER_WRITE);
     i2cWriteByte(I2CRTC_STOPBIT);
+
     i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGSEC, I2CMASTER_WRITE);
     i2cWriteByte(0x1);
     i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGMIN, I2CMASTER_WRITE);
     i2cWriteByte(0x1);
     i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGHR, I2CMASTER_WRITE);
-    i2cWriteByte(0x11);
     i2cWriteByte(0x15);
+
+    portEXIT_CRITICAL ();
+    // rtc_sleep();
 }
 
-int rtc_get() {
-    i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGSEC, I2CMASTER_READ);
-    int rtcsec = i2cReadByte();
-      
-    i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGMIN, I2CMASTER_READ);
-    int rtcmin = i2cReadByte();
+int rtc_read(struct tm *curr_time, unsigned int *milliseconds) {
+    TRACE_INFO("rtc_read()\r\n");
 
-    i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGHR, I2CMASTER_READ);
-    int rtchour = i2cReadByte();
+    // rtc_wake();
+    portENTER_CRITICAL ();
+
+    if (milliseconds) {
+        *milliseconds = 0;
+    }
+
+    /*
+       i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGSEC, I2CMASTER_READ);
+       int rtcsec = i2cReadByte();
+
+       i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGMIN, I2CMASTER_READ);
+       int rtcmin = i2cReadByte();
+
+       i2cMasterConf(I2CRTC_PHY_ADDR, 1, I2CRTC_REGHR, I2CMASTER_READ);
+       int rtchour = i2cReadByte();
+       */
+    uint8_t b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGSEC);
+    curr_time->tm_sec = BCD2BIN(b & 0x7f);
+    b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGMIN);
+    curr_time->tm_min = BCD2BIN(b & 0x7f);
+    b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGHR);
+    curr_time->tm_hour = BCD2BIN(b & 0x3f);
+    b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGDATE);
+    curr_time->tm_mday = BCD2BIN(b & 0x3f);
+    b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGCENMON);
+    uint8_t century = (b & 0xc0) >> 6;
+/*
+century:
+0 0 2000 (leap year; (y % 4 == 0) && ((y % 100 != 0) || (y % 400 == 0))
+0 1 2100
+1 0 2200
+1 1 2300
+*/
+
+    curr_time->tm_mon = BCD2BIN(b & 0x1f) - 1;
+    b = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGYEAR);
+    curr_time->tm_year = 2000 + century * 100 + BCD2BIN(b) - 1900;
+    //curr_time->tm_wday = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGDAY);
+    //curr_time->tm_yday = i2cMasterRead(I2CRTC_PHY_ADDR, 1, I2CRTC_REGx);
+    curr_time->tm_isdst = 0;
+
+    portEXIT_CRITICAL ();
+    // rtc_sleep();
+
+    TRACE_INFO("rtc: %02d:%02d:%02d\r\n", curr_time->tm_hour, curr_time->tm_min, curr_time->tm_sec);
+}
+
+time_t rtc_get_epoch_seconds (unsigned int *milliseconds) {
+  struct tm tm;
+
+  rtc_read (&tm, milliseconds);
+  return mktime (&tm);
+}
+
+void rtc_set_epoch_seconds (time_t now) {
+  struct tm tm;
+
+  localtime_r (&now, &tm);
+  rtc_write (&tm);
+}
+
+int rtc_set_alarm (struct tm *tm)
+{
+    if (tm && (mktime (tm) < time (NULL)))
+        return -1;
+
+    return 0;
+}
+
+struct tm *rtc_get_alarm (struct tm *tm) {
+}
+
+time_t rtc_get_alarm_epoch_seconds (void) {
+    struct tm tm;
+
+    return mktime (rtc_get_alarm (&tm));
+}
+
+int rtc_periodic_alarm (int mode) {
+    return 0;
 }
--- core/screen/screen.c
+++ core/screen/screen.c
@@ -13,7 +13,7 @@
  *
  *
  *   
-*/
+ */
 
 #include <stdio.h>
 #include "hardware_conf.h"
@@ -21,10 +21,7 @@
 #include <utils/delay.h>
 #include <peripherals/oled/oled.h>
 #include "screen.h"
-
-
-
-
+#include "debug/trace.h"
 
 scr_coord_t scrscrX1;
 scr_coord_t scrscrX2;
@@ -38,147 +35,174 @@ extern scr_buf_t * scrbuf;
 //note: requires the SMC pins (AD0.., D0..D15, R/W) already configured!
 int scrInit(void)
 {
-  //init hw, switch power on and clear display
-  //(void) oledInitHw();  
-  
-  //configure screen window
-  scrscrX1=0; scrscrX2=OLED_RESOLUTION_X-1; 
-  scrscrY1=0; scrscrY2=OLED_RESOLUTION_Y-1;
-  
-  //oledScreen(scrscrX1,scrscrY1,scrscrX2,scrscrY2);
-  scrbuf = NULL;  
-  scrrot = 0;
-  scrmirror = 0;
-  
-  return 0;
+    //init hw, switch power on and clear display
+    //(void) oledInitHw();  
+
+    //configure screen window
+    scrscrX1=0; scrscrX2=OLED_RESOLUTION_X-1; 
+    scrscrY1=0; scrscrY2=OLED_RESOLUTION_Y-1;
+
+    //oledScreen(scrscrX1,scrscrY1,scrscrX2,scrscrY2);
+    scrbuf = NULL;  
+    scrrot = 0;
+    scrmirror = 0;
+
+    return 0;
 }
 
 void scrWriteRect(scr_coord_t left_x, scr_coord_t top_y, scr_coord_t right_x, scr_coord_t bot_y, scr_color_t color)
 {
-  scr_coord_t x1,x2,y1,y2;
-  volatile oled_access_fast *pFastOLED;
-  oled_access_fast col;
-  volatile oled_access_cmd *pOLED;
-  uint32_t writes,i;
-    
-   
-  if (scrbuf==NULL)
-  {
-    //direct to screen
-    if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
-    if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
-    if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
-    if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
-    
-    //clear screen (blank, black)
-    oledWriteCommand(MX1_ADDR, x1);
-    oledWriteCommand(MY1_ADDR, y1);
-    oledWriteCommand(MX2_ADDR, x2);
-    oledWriteCommand(MY2_ADDR, y2);
-    oledWriteCommand(MEMORY_ACCESSP_X, x1);
-    oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
-    pOLED=OLED_CMD_BASE;  
-    *pOLED = (OLED_DDRAM<<1); //bit align          
-    pFastOLED = OLED_PARAM_BASE;
-    writes = (((x2-x1+1)*(y2-y1+1))/8)+1;   
-    col = rgb2w((color&0xff),(color>>8)&0xff,(color>>16)&0xff);
-    col = col | (col<<32);
-    //this is ok - if rectangle is < 8pix overwrites picture many times          
-    for (i=0;i<(writes);(i++))
-    {    
-      *pFastOLED = col;
-      *pFastOLED = col;
-      *pFastOLED = col;
-      *pFastOLED = col;            
+    scr_coord_t x1,x2,y1,y2;
+    volatile oled_access_fast *pFastOLED;
+    oled_access_fast col;
+    volatile oled_access_cmd *pOLED;
+    uint32_t writes,i;
+
+
+    if (scrbuf==NULL)
+    {
+        //direct to screen
+        if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
+        if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
+        if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
+        if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
+
+        //clear screen (blank, black)
+        oledWriteCommand(MX1_ADDR, x1);
+        oledWriteCommand(MY1_ADDR, y1);
+        oledWriteCommand(MX2_ADDR, x2);
+        oledWriteCommand(MY2_ADDR, y2);
+        oledWriteCommand(MEMORY_ACCESSP_X, x1);
+        oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
+        pOLED=OLED_CMD_BASE;  
+        *pOLED = (OLED_DDRAM<<1); //bit align          
+        pFastOLED = OLED_PARAM_BASE;
+        writes = (((x2-x1+1)*(y2-y1+1))/8)+1;   
+        col = rgb2w((color&0xff),(color>>8)&0xff,(color>>16)&0xff);
+        col = col | (col<<32);
+        //this is ok - if rectangle is < 8pix overwrites picture many times          
+        for (i=0;i<(writes);(i++))
+        {    
+            *pFastOLED = col;
+            *pFastOLED = col;
+            *pFastOLED = col;
+            *pFastOLED = col;            
+        }
+
+    } else {
+        //write to screen memory buffer 
+
     }
-  
-  } else {
-    //write to screen memory buffer 
-   
-  }
 }
 
 
 void scrWriteBitmap(scr_coord_t left_x, scr_coord_t top_y, scr_coord_t right_x, scr_coord_t bot_y, scr_bitmapbuf_t *buf)
 {
-  scr_coord_t x1,x2,y1,y2;
-  //volatile oled_access_fast *pFastOLED;  
-  volatile oled_access_cmd *pCMDOLED;
-  volatile oled_access *pOLED;
-  uint32_t writes,i;
-  
-  if (scrbuf==NULL)
-  {
-    if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
-    if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
-    if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
-    if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
+    scr_coord_t x1,x2,y1,y2;
+    //volatile oled_access_fast *pFastOLED;  
+    volatile oled_access_cmd *pCMDOLED;
+    volatile oled_access *pOLED;
+    uint32_t writes,i;
 
-    //direct to screen
-    //clear screen (blank, black)
-    oledWriteCommand(MX1_ADDR, x1);
-    oledWriteCommand(MY1_ADDR, y1);
-    oledWriteCommand(MX2_ADDR, x2);
-    oledWriteCommand(MY2_ADDR, y2);
-    oledWriteCommand(MEMORY_ACCESSP_X, x1);
-    oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
-    pCMDOLED=OLED_CMD_BASE;  
-    *pCMDOLED = (OLED_DDRAM<<1); //bit align
-           
-    pOLED = OLED_PARAM_BASE;
-    writes = (((x2-x1+1)*(y2-y1+1)));   
-              
-    for (i=0;i<(writes);(i++))
-    {          
-      *pOLED = rgb2w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);;                  
-      //*pOLED = rgb2w(0xff,0xff,0xff);
+    if (scrbuf==NULL)
+    {
+        if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
+        if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
+        if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
+        if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
+
+        //direct to screen
+        //clear screen (blank, black)
+        oledWriteCommand(MX1_ADDR, x1);
+        oledWriteCommand(MY1_ADDR, y1);
+        oledWriteCommand(MX2_ADDR, x2);
+        oledWriteCommand(MY2_ADDR, y2);
+        oledWriteCommand(MEMORY_ACCESSP_X, x1);
+        oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
+        pCMDOLED=OLED_CMD_BASE;  
+        *pCMDOLED = (OLED_DDRAM<<1); //bit align
+
+        pOLED = OLED_PARAM_BASE;
+        writes = (((x2-x1+1)*(y2-y1+1)));   
+
+        for (i=0;i<(writes);(i++))
+        {          
+            *pOLED = rgb2w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);;                  
+            //*pOLED = rgb2w(0xff,0xff,0xff);
+        }
+
+    } else {
+        //write to screen memory buffer 
+
     }
-  
-  } else {
-    //write to screen memory buffer 
-  
-  }
 }
 
 void scrWriteBitmapRGBA(scr_coord_t left_x, scr_coord_t top_y, scr_coord_t right_x, scr_coord_t bot_y, scr_bitmapbuf_t *buf)
 {
-  scr_coord_t x1,x2,y1,y2;
-  //volatile oled_access_fast *pFastOLED;  
-  volatile oled_access_cmd *pCMDOLED;
-  volatile oled_access *pOLED;
-  uint32_t writes,i;
-  
-  if (scrbuf==NULL)
-  {
-    if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
-    if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
-    if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
-    if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
+    scr_coord_t x1,x2,y1,y2;
+    //volatile oled_access_fast *pFastOLED;  
+    volatile oled_access_cmd *pCMDOLED;
+    volatile oled_access *pOLED;
+    uint32_t writes,i;
 
-    //direct to screen
-    //clear screen (blank, black)
-    oledWriteCommand(MX1_ADDR, x1);
-    oledWriteCommand(MY1_ADDR, y1);
-    oledWriteCommand(MX2_ADDR, x2);
-    oledWriteCommand(MY2_ADDR, y2);
-    oledWriteCommand(MEMORY_ACCESSP_X, x1);
-    oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
-    pCMDOLED=OLED_CMD_BASE;  
-    *pCMDOLED = (OLED_DDRAM<<1); //bit align
-           
-    pOLED = OLED_PARAM_BASE;
-    writes = (((x2-x1+1)*(y2-y1+1)));   
-              
-    for (i=0;i<(writes);(i++))
-    {          
-// 24 - 31b - Alpha
-      //*pOLED = rgb2w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);;                  
-// [RGB] 8b -> 6b
-      *pOLED = rgb2w((buf[i]&0xff)>>2,((buf[i]>>8)&0xff)>>2,((buf[i]>>16)&0xff)>>2);                  
+    if (scrbuf==NULL)
+    {
+        if (left_x>=0) x1 = left_x; else x1=0; if (left_x<OLED_RESOLUTION_X) x1 = left_x; else x1=OLED_RESOLUTION_X-1;
+        if (right_x>=0) x2 = right_x; else x2=0; if (right_x<OLED_RESOLUTION_X) x2 = right_x; else x2=OLED_RESOLUTION_X-1;
+        if (top_y>=0) y1 = top_y; else y1=0; if (top_y<OLED_RESOLUTION_Y) y1 = top_y; else y1=OLED_RESOLUTION_Y-1;
+        if (bot_y>=0) y2 = bot_y; else y2=0; if (bot_y<OLED_RESOLUTION_Y) y2 = bot_y; else y2=OLED_RESOLUTION_Y-1;
+
+        //direct to screen
+        //clear screen (blank, black)
+        oledWriteCommand(MX1_ADDR, x1);
+        oledWriteCommand(MY1_ADDR, y1);
+        oledWriteCommand(MX2_ADDR, x2);
+        oledWriteCommand(MY2_ADDR, y2);
+        oledWriteCommand(MEMORY_ACCESSP_X, x1);
+        oledWriteCommand(MEMORY_ACCESSP_Y, y1);  
+        pCMDOLED=OLED_CMD_BASE;  
+        *pCMDOLED = (OLED_DDRAM<<1); //bit align
+
+        pOLED = OLED_PARAM_BASE;
+        writes = (((x2-x1+1)*(y2-y1+1)));   
+
+// Bitmap RGBA (6b channel)
+// 10987654321098765432109876543210
+// 32103210321032103210321032103210
+// AAAAAAAABBBBBB--GGGGGG--RRRRRR--
+// 76543210765432--765432--765432--
+
+// OLED Controller Write
+// 10987654321098765432109876543210
+// 32103210321032103210321032103210
+// -------GGGBBBBBB-------RRRRRRGGG
+// -------210543210-------543210543
+
+#define rgb21w(r,g,b) ((((b)&0x3F)<<16)|(((g)&0x7)<<22)|(((g)>>3)&0x7)|(((r)&0x3f)<<3))
+#define rgb62w(r,g,b) ((((b)&0xfc)<<14)|(((g)&0x1c)<<20)|(((g)>>5)&0x7)|(((r)&0xfc)<<1))
+#define rgba2w(rgba)    ((((rgba) & 0xfc) << 1) | (((rgba) & 0xfc0000) >> 2) | (((rgba) & 0x1c00) << 12) | (((rgba) & 0xe000) >> 13))
+
+/*
+        TRACE_INFO("%x\r\n", rgb2w(0xfc>>2, 0xfc>>2, 0xfc>>2));
+        TRACE_INFO("%x\r\n", rgb62w(0xfc, 0xfc, 0xfc));
+        TRACE_INFO("%x\r\n", rgba2w(0xfcfcfc));
+*/
+        for (i=0;i<(writes);(i++))
+        {          
+            // 24 - 31b - Alpha
+            //*pOLED = rgb2w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);                  
+            //*pOLED = rgb21w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);                  
+            //register uint32_t rgba = (buf[i] & 0xfcfcfc) >> 2;
+            //*pOLED = rgb2w((rgba&0xff),(rgba>>8)&0xff,(rgba>>16)&0xff);                  
+            //*pOLED = rgb62w((buf[i]&0xff),(buf[i]>>8)&0xff,(buf[i]>>16)&0xff);                  
+            // [RGB] 8b -> 6b
+            //*pOLED = rgb2w((buf[i]&0xff)>>2,((buf[i]>>8)&0xff)>>2,((buf[i]>>16)&0xff)>>2);                  
+            //*pOLED = buf[i];
+            *pOLED = rgba2w(buf[i]);
+        }
+
+    } else {
+        //write to screen memory buffer 
+
     }
-  
-  } else {
-    //write to screen memory buffer 
-  
-  }
 }
