a4eda17d7ece0251d3939b996a025e4c590b8051
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-02-06 15:36:16 +0000

    Menu / widget scaffolding.

--- trunk/images/lua/sd_card/_SYS/apps/core/task_manager.lua
+++ trunk/images/lua/sd_card/_SYS/apps/core/task_manager.lua
@@ -11,6 +11,9 @@ local app_index = 1
 local function _app_to_front(new_app)
 	local previous_app = dynawa.app.in_front --Can be nil immediately after boot!
 	dynawa.app.in_front = new_app
+	if previous_app == new_app then --do nothing
+		return
+	end
 	new_app.in_front = true
 	if previous_app then
 		previous_app.in_front = nil
@@ -32,6 +35,12 @@ local function switch_app(event)
 	end
 end
 
+local function app_to_front(event)
+	local app = assert(event.app)
+	assert(app.tasks,"This is not an app - it has no tasks")
+	_app_to_front(app)
+end
+
 local function sender_to_front(event)
 	local task = assert(event.sender,"No sender found in app_to_front event")
 	local app = assert(task.app)
@@ -40,4 +49,5 @@ end
 
 dynawa.event.receive{event="button_down", callback=switch_app}
 dynawa.event.receive{event="me_to_front", callback=sender_to_front}
+dynawa.event.receive{event="app_to_front", callback=app_to_front}
 
--- trunk/images/lua/sd_card/_SYS/apps/widgets/main.lua
+++ trunk/images/lua/sd_card/_SYS/apps/widgets/main.lua
@@ -1,20 +1,46 @@
-my.app.name = "System Widgets"
+my.app.name = "System Widget"
 
-local scr = dynawa.bitmap.new(160,128,255,255,0)
+local current_widget = nil
+local inactive_mask = dynawa.bitmap.from_png_file(my.dir.."inactive_mask.png")
+
+local unique_id_number = 0
+my.globals.unique_id = function()
+	unique_id_number = unique_id_number + 1
+	return "widget_"..unique_id_number
+end
+
+dofile (my.dir.."menu.lua")
 
 local function new_widget(event)
 	local app=assert(event.sender.app)
-	local scr = app.screen or dynawa.bitmap.new(160,128,255,255,0)
-	local widget = dynawa.bitmap.new(50,30,0,0,80)
-	scr = dynawa.bitmap.combine(scr,widget,20,10,true)
+	local scr = assert(app.screen)
+	scr = dynawa.bitmap.combine(scr,inactive_mask,0,0,true)
+	local widget = my.globals.menu.new()
+	widget.app = app
+	dynawa.bitmap.combine(scr,widget.bitmap,5,5)
+	current_widget = widget
 	dynawa.event.send{type="display_bitmap", bitmap=scr}
 	dynawa.event.send{type="me_to_front"}
 end
 
-local function now_in_back(event)
+local function widget_cancelled()
+	if not current_widget then
+		return
+	end
+	dynawa.event.send{type="widget_done", status = "cancelled", id=current_widget.id, receiver = current_widget.app}
+	dynawa.event.send{type="app_to_front", app = current_widget.app}
 	dynawa.event.send{type="display_bitmap", bitmap = nil}
+	current_widget = nil
+end
+
+local function button_down(event)
+	if event.button == "CANCEL" or event.button == "SWITCH" then
+		widget_cancelled()
+		return
+	end
 end
 
 dynawa.event.receive{event="new_widget", callback=new_widget}
-dynawa.event.receive{event="you_are_now_in_back", callback=now_in_back}
+dynawa.event.receive{event="you_are_now_in_back", callback=widget_cancelled}
+dynawa.event.receive{event="button_down", callback=button_down}
 
