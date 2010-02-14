4722ef8eb6d7e8f43451f15b9b10bb23664bd9f1
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-02-14 17:12:26 +0000

    dir_stat() error test case

--- trunk/images/lua/sd_card/_SYS/apps/superman/main.lua
+++ trunk/images/lua/sd_card/_SYS/apps/superman/main.lua
@@ -1,31 +1,43 @@
 ----SuperMan
 
+local function get_menu_for_location(url)
+	assert(url)
+	local where, rest = url:match("(.-):(.*)")
+	local fn = my.globals.menus[where]
+	assert(fn, "Unknown go_to address: "..where)
+	return assert(fn(rest),"No menu returned for location "..url)
+end
+
+
 my.globals.show_menu = function(args)
 	args = args or {}
 	local location = args.location
 	local menu
 	if not location then --root menu
-		menu = {
+		menu = {}
+		menu.items = {
 			{text = "Adjust clock", location = "adjust_clock:"},
-			{text = "File browser", location = "file_browser:"},
+			{text = "File browser", location = "file_browser:/"},
 			{text = "Nothing1"},
 			{text = "Nothing2"},
 		}
 	else
-		error("Unknown SuperMan location")
+		menu = get_menu_for_location(location)
 	end
 	
 	local menu2 = {items={}}
 	menu2.banner = menu.banner or "SuperMan"
 	menu2.border_color = {200,200,0}
 	menu2.fullscreen = true
-	for i,item in ipairs(menu) do
+	for i,item in ipairs(menu.items) do
 		local item2 = {text = assert(item.text)}
+		--log("Added SuperMan menu text: "..item2.text)
 		if item.location then
 			item2.value={go_to=item.location}
 		end
 		table.insert(menu2.items, item2)
 	end
+	assert(#menu2.items > 0, "No menu items in SuperMan menu")
 	dynawa.event.send{type="new_widget", menu = menu2}
 end
 
@@ -35,9 +47,18 @@ end
 
 local function widget_result(event)
 	log("Superman got result")
-	for i = 1,1000 do
-		dynawa.busy()
+	local val = event.value
+	if not val then
+		for i = 1,1000 do
+			dynawa.busy()
+		end
+		return
 	end
+	if val.go_to then
+		my.globals.show_menu{location = val.go_to}
+		return
+	end
+	log("Confirmation value not handled by SuperMan")
 	--dynawa.event.send{type="close_widget"}
 end
 
--- trunk/images/lua/sd_card/_SYS/apps/superman/menus.lua
+++ trunk/images/lua/sd_card/_SYS/apps/superman/menus.lua
@@ -1,3 +1,23 @@
 --menus for SuperMan
 my.globals.menus = {}
-
+my.globals.menus.file_browser = function(dir)
+	assert(type(dir)=="string")
+	assert(#dir >= 1)
+	log("opening dir "..dir)
+	local dirstat = dynawa.file.dir_stat(dir)
+	local menu = {banner = "Directory: "..dir, items={}}
+	if not dirstat then
+		table.insert(menu.items,{text="(Invalid directory)"})
+	else
+		for k,v in pairs(dirstat) do
+			local txt = k.." ("..v..")"
+			log("Adding dirstat item: "..txt)
+			local location
+			if v == "dir" then
+				location = "file_browser:"..dir..k.."/"
+			end
+			table.insert(menu.items,{text=txt,location=location})
+		end
+	end
+	return menu
+end
