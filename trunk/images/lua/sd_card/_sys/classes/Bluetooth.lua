4efc9185ffcf616e0abae46aba124e940cfcc0df
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-04-19 21:42:26 +0000

    BT on/off bug

--- trunk/images/lua/sd_card/_sys/classes/Bluetooth.lua
+++ trunk/images/lua/sd_card/_sys/classes/Bluetooth.lua
@@ -2,6 +2,25 @@ local class = Class("Bluetooth", Class.EventSource)
 
 function class:_init()
 	Class.EventSource._init(self,"bluetooth")
+	local cmd = {
+		OPEN = 1,
+		CLOSE = 2,
+		SET_LINK_KEY = 3,
+		INQUIRY = 4,
+		SOCKET_NEW = 100,
+		SOCKET_CLOSE = 101,
+		SOCKET_BIND = 102,
+		FIND_SERVICE = 200,
+		LISTEN = 300,
+		CONNECT = 301,
+		SEND = 400,
+	}
+	self.cmd = {}
+	for key, val in pairs(cmd) do
+		self.cmd[key] = function(self, ...)
+			return dynawa.bt.cmd(val, ...)
+		end
+	end
 end
 
 local events = {
