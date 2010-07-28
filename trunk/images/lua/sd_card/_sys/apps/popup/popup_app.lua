aa4f1b54834e32538ae5093e3fc06519c3466f33
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-07-28 20:58:53 +0000

    Pressing CONFIRM when message popup is displayed opens details of this message in its folder.

--- trunk/images/lua/sd_card/_sys/apps/popup/popup_app.lua
+++ trunk/images/lua/sd_card/_sys/apps/popup/popup_app.lua
@@ -53,6 +53,7 @@ function app:open(args)
 	dynawa.bitmap.combine(screen, bmp, start_w, start_h)
 	self.window:show_bitmap(screen)
 	self.window:push()
+	self.window.on_confirm = args.on_confirm
 	self.timestamp = dynawa.ticks()
 	if autoclose then 
 		dynawa.devices.timers:timed_event{delay = autoclose, receiver = self, window_id = assert(self.window.id)}
@@ -75,7 +76,11 @@ end
 function app:handle_event_button(event)
 	if assert(event.action) == "button_down" and dynawa.ticks() - self.timestamp > 250 then
 		if event.button == "confirm" or event.button == "cancel" then
+			local on_confirm = self.window.on_confirm
 			self:switching_to_back()
+			if on_confirm and event.button == "confirm" then
+				on_confirm()
+			end
 		end
 	end
 end
