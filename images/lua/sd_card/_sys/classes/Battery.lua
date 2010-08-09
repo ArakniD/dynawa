2d48952d829369804d280eb3948296b1dff3e610
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-08-09 07:58:53 +0000

    Added battery state log to file /battery_log.txt. Battery indicator changed to 0% = 3000mA, 100% = 4150mA (linear). Critical voltage remains at 3500 mA. Battery polling interval changed to 60 seconds.

--- images/lua/sd_card/_sys/classes/Battery.lua
+++ images/lua/sd_card/_sys/classes/Battery.lua
@@ -1,16 +1,24 @@
 local class = Class("Battery", Class.EventSource)
 
+local function log_file(txt)
+	local time = os.date("*t")
+	local timetxt = string.format("%02d:%02d:%02d", time.hour, time.min, time.sec)
+	local fd = assert(io.open("/battery_log.txt","a"))
+	assert(fd:write(timetxt.." "..txt.."\n"))
+	fd:close()
+end
 
 function class:_init()
 	Class.EventSource._init(self,"battery")
 	self.last_status = false
 	self.critical_percentage = self:voltage_to_percent(3500)
 	dynawa.devices.timers:timed_event{delay = 10000, receiver = self}
+	log_file("REBOOT")
 end
 
 function class:voltage_to_percent(v)
-	--Linear, for now...
-	local pct = (v - 3400) / 8
+	--Linear, for now... 3000 - 4150
+	local pct = (v - 3000) / 11.5
 	pct = math.floor(pct + 0.5)
 	if pct < 0 then
 		return 0
@@ -33,7 +41,9 @@ function class:status()
 		status.critical = true
 	end
 	self.last_status = {timestamp = status.timestamp, voltage = status.voltage, percentage = status.percentage}
-	log("Voltage = "..status.voltage .. " ("..status.percentage.."%)")
+	local logtxt = "Voltage = "..status.voltage .. " ("..status.percentage.."%)"
+	log(logtxt)
+	log_file(logtxt)
 	return status
 end
 
@@ -48,7 +58,7 @@ function class:handle_event_timed_event(event)
 		self:broadcast_update(status)
 	end
 	--#todo Dynamic delay?
-	local delay = 10000
+	local delay = 60000
 	dynawa.devices.timers:timed_event{delay = delay, receiver = self}
 end
 
