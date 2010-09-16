1ad1b1306c80df28370012bcc08c3b651b658962
nalim68 <nalim68@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-09-16 07:05:57 +0000

    - audio testin

--- core/dynawa/audio.c
+++ core/dynawa/audio.c
@@ -5,7 +5,8 @@
 
 #define SAMPLE_LEN 60000
 
-static uint16_t sample[SAMPLE_LEN];
+uint16_t sample[SAMPLE_LEN];
+uint16_t rcv_sample[SAMPLE_LEN];
 
 void audioIsr_Wrapper( );
 
@@ -21,17 +22,48 @@ void audio_play() {
     for(i = 0; i < SAMPLE_LEN; i++) {
         //sample[i] = p[i] & 0x3ffc;
         sample[i] = high ? 0x3ffc : 0;
-        if (!(i % 10))
+        //sample[i] = high ? 0x2000 : 0;
+        //sample[i] = high ? 0x0004 : 0;
+        //sample[i] = 0xaaaa;
+        if (!(i % 2))
             high = !high; 
+        rcv_sample[i] = 0x6969;
     }
 
     // Configure and enable the SSC interrupt
     AIC_ConfigureIT(BOARD_DAC7311_SSC_ID, 0, audioIsr_Wrapper);
     AIC_EnableIT(BOARD_DAC7311_SSC_ID);
 
-    DAC7311_Enable(10000, 16 / 8, MCK);
+    DAC7311_Enable(10000, 2, MCK);
 
     audio_start = Timer_tick_count();
+#if 1  // DMA Playback
     SSC_WriteBuffer(BOARD_DAC7311_SSC, (void *) sample, SAMPLE_LEN);
+    SSC_WriteBuffer(BOARD_DAC7311_SSC, (void *) sample, SAMPLE_LEN); // 2nd DMA buffer
     SSC_EnableInterrupts(BOARD_DAC7311_SSC, AT91C_SSC_TXBUFE | AT91C_SSC_ENDTX);
+#elif 0 // CPU Playback
+    while (1) {
+        for(i=0; i<SAMPLE_LEN; i++) {
+            SSC_Write(BOARD_DAC7311_SSC, sample[i]);
+        }
+    }
+#elif 0 // loopback test, DMA
+    SSC_ReadBuffer(BOARD_DAC7311_SSC, (void *) rcv_sample, 10);
+    SSC_EnableInterrupts(BOARD_DAC7311_SSC, AT91C_SSC_TXBUFE | AT91C_SSC_ENDTX | AT91C_SSC_RXBUFF | AT91C_SSC_ENDRX);
+    for(i=0; i<10; i++) {
+        //SSC_Write(BOARD_DAC7311_SSC, i + 1);
+        SSC_Write(BOARD_DAC7311_SSC, 0xaaaa);
+    }
+#elif 0 // loopback test, DMA
+    SSC_ReadBuffer(BOARD_DAC7311_SSC, (void *) rcv_sample, 10);
+    SSC_WriteBuffer(BOARD_DAC7311_SSC, (void *) sample, 10);
+    SSC_EnableInterrupts(BOARD_DAC7311_SSC, AT91C_SSC_TXBUFE | AT91C_SSC_ENDTX | AT91C_SSC_RXBUFF | AT91C_SSC_ENDRX);
+#elif 0 // loopback test, CPU
+    for(i=0; i<10; i++) {
+        //SSC_Write(BOARD_DAC7311_SSC, i + 1);
+        SSC_Write(BOARD_DAC7311_SSC, 0xaaaa);
+        uint32_t w = SSC_Read(BOARD_DAC7311_SSC);
+        TRACE_INFO("ssc %d %x\r\n", i, w);
+    }
+#endif
 }
--- images/lua/image.c
+++ images/lua/image.c
@@ -211,12 +211,12 @@ void Run( ) // this task gets called as soon as we boot up.
 #endif
     //Task_create( lua_event_loop, "lua", TASK_LUA_STACK, TASK_LUA_PRI, NULL );
 
-/*
+#if 1
     audio_play();
     while(1) {
         asm volatile ("nop");
     };
-*/
+#endif
     xTaskCreate(lua_event_loop, (signed char*)"lua", TASK_STACK_SIZE(TASK_LUA_STACK), NULL, TASK_LUA_PRI, NULL);
 
 #if 0
