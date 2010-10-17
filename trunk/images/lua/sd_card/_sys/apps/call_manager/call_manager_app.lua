504b6c54885ad628a982147994d78730c4285083
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-10-17 21:43:08 +0000

    - Accepted / rejected incoming calls correctly logged to Inbox. - Accepted calls are automatically marked as "read" and include the duration info.

--- trunk/images/lua/sd_card/_sys/apps/call_manager/call_manager_app.lua
+++ trunk/images/lua/sd_card/_sys/apps/call_manager/call_manager_app.lua
@@ -5,6 +5,7 @@ app.id = "dynawa.call_manager"
 
 function app:start()
 	self.phones = {} --indexed by bdaddr
+	self.events = Class.EventSource("call_manager")
 	dynawa.app_manager:after_app_start("dynawa.dyno",function (dyno)
 		dyno.events:register_for_events(self, function(ev)
 			if ev.type ~= "dyno_data_from_phone" then
@@ -19,17 +20,59 @@ end
 function app:handle_event_dyno_data_from_phone(event)
 	local data = assert(event.data)
 	if data.command == "incoming_call" then
-		self.phones[data.bdaddr] = {status = "ringing"}
+		self.phones[data.bdaddr] = {status = "ringing", since = dynawa.ticks(), data = data}
 		log("--INCOMING CALL")
 		self:incoming_call_popup(data)
-		--show window
+		return
 	elseif data.command == "call_start" then
 		log("--CALL START")
-		self.phones[data.bdaddr] = {status = "in_progress", since = dynawa.ticks()}
+		local phone = self.phones[data.bdaddr]
+		if not phone then
+			--This must be an outgoing call because we have no record of this phone ringing. Ignore it.
+			return
+		end
+		if phone.status ~= "ringing" then
+			--Phone has a record but indicates other status than "ringing". Weird. Ignore & clear it.
+			self.phones[data.bdaddr] = nil
+			return
+		end
+		--Phone is ringing.
+		if dynawa.ticks() - phone.since > 300000 then
+			--Phone was ringing for more than 5 minutes before being picked up. This cannot be right.
+			self.phones[data.bdaddr] = nil
+			return
+		end
+		phone.status = "incoming_call"
+		phone.since = dynawa.ticks()
+		return
 	else
 		assert(data.command == "call_end")
 		log("--CALL END")
+		local phone = self.phones[data.bdaddr]
+		if not phone then
+			return
+		end
+		if phone.status ~= "incoming_call" then
+			self.phones[data.bdaddr] = nil
+			if phone.status == "ringing" and (dynawa.ticks() - phone.since < 300000) then
+				--Generate voicecall event. It's MISSED CALL because duration == nil
+				self.events:generate_event{type="incoming_call", data = assert(phone.data)}
+			end
+			return
+		end
+		local duration = math.floor((dynawa.ticks()-phone.since)/1000)
+		if duration < 1 then
+			duration = 1
+		end
+		self.phones[data.bdaddr] = nil
+		local call_data = assert(phone.data)
+		call_data.duration = string.format("%d:%02d",math.floor(duration / 60), duration % 60)
+		--log("Call duration: "..duration)
+		--generate voicecall event. It's ACCEPTED CALL because duration ~= nil
+		self.events:generate_event{type="incoming_call", data = call_data}
+		return
 	end
+	error("WTF")
 end
 
 function app:phone_action(action,bdaddr)
@@ -45,6 +88,7 @@ function app:phone_action(action,bdaddr)
 end
 
 function app:incoming_call_popup(data)
+	dynawa.busy()
 	local rows = {"Call: "..(data.contact_name or data.contact_phone)}
 	for i, row in ipairs(rows) do
 		if type(row) == "string" then
