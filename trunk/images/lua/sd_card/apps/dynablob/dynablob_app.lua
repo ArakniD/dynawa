f76237bdc6b7e9ae2c52c6377bd2a782339f5a06
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-10-18 23:33:28 +0000

    - First DynaBlob animation test.

--- trunk/images/lua/sd_card/apps/dynablob/dynablob_app.lua
+++ trunk/images/lua/sd_card/apps/dynablob/dynablob_app.lua
@@ -2,26 +2,74 @@ app.name = "DynaBlob"
 app.id = "fuxoft.dynablob"
 
 function app:start()
-	self.sprites = {}
-	self.parts_bitmap = assert(dynawa.bitmap.from_png_file(self.dir.."parts.png"))
+	self.window = self:new_window()
 	self:init_indices()
 end
 
+function app:switching_to_front()
+	self.window:push()
+	self.run_id = dynawa.unique_id()
+	self:animate(0)
+end
+
+function app:animate(count)
+	self.window:fill()
+	self:put_sprite("testmark",0,0)
+	self:put_sprite("blob4",80,120)
+	self:put_sprite("blob3",80,105)
+	self:put_sprite("blob3",80,90)
+	self:put_sprite("blob2",80,80)
+	self:put_sprite("eye1",80-10,80)
+	self:put_sprite("eye1",80+10,80)
+	if math.random(5) == 1 then
+		self:put_sprite("mouth1",80,100)
+	else
+		self:put_sprite("mouth2",80,100)
+	end
+	dynawa.devices.timers:timed_event{delay = 200, receiver = self, run_id = self.run_id}
+end
+
+function app:handle_event_timed_event(event)
+	if self.run_id ~= event.run_id then
+		return
+	end
+	if not self.window.in_front then
+		self.run_id = nil
+		return
+	end
+	self:animate(0)
+end
+
+function app:put_sprite(sprid, cx, cy, window)
+	window = window or self.window
+	local sprite = self.sprites[sprid]
+	if not sprite then
+		error("Uknown sprite id: "..sprid)
+	end
+	local dx = math.random(5) - 3
+	local dy = math.random(5) - 3
+	window:show_bitmap_at(sprite.bitmap, cx - sprite.half_size.w + dx, cy - sprite.half_size.h + dy)
+end
+
 function app:init_indices()
+	local parts = assert(dynawa.bitmap.from_png_file(self.dir.."parts.png"))
 	local indices = {
-		["blob/1"] = {9,63,49,64},
-		["blob/2"] = {6,100,58,149},
-		["blob/3"] = {68,66,141,114},
-		["blob/4"] = {75,127,145,150},
-		["eye"] = {0,0,6,6},	
+		["testmark"] = {0,0,4,4},
+		["blob1"] = {9,63,49,64},
+		["blob2"] = {6,100,58,149},
+		["blob3"] = {68,66,141,114},
+		["blob4"] = {75,127,145,150},
+		["eye1"] = {15,0,24,10},
+		["mouth1"] = {0,19,14,29},
+		["mouth2"] = {15,23,29,27},
 	}
-	self.sprite_indices = {}
+	self.sprites = {}
 	for id, ind in pairs(indices) do
 		local x,y = ind[1],ind[2]
-		local w,h = ind[3] - x, ind[4] - y
+		local w,h = ind[3] - x + 1, ind[4] - y + 1
 		assert(w>0)
 		assert(h>0)
-		self.sprite_indices[id] = {top_left = {x=x,y=y}, size = {w=w,h=h}, center = {x=math.floor(w/2) + x, y=math.floor(h/2) + y}}
+		self.sprites[id] = {bitmap = dynawa.bitmap.copy(parts,x,y,w,h),size = {w=w,h=h}, half_size = {w=math.floor(w/2), h=math.floor(h/2)}}
 	end
 end
 
