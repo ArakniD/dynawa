f5e6949464025ee56732a790c89a5093503d7a3d
fuka.frantisek <fuka.frantisek@33c7e71e-f84e-11de-ab06-dd2c9883db02>
2010-11-13 23:40:59 +0000

    - Dyno error test case (http_tester_app.lua)

--- images/lua/sd_card/_sys/apps/http_request/http_request_app.lua
+++ images/lua/sd_card/_sys/apps/http_request/http_request_app.lua
@@ -2,25 +2,30 @@ app.name = "HTTP Request Service"
 app.id = "dynawa.http_request"
 
 function app:start()
+	self.requests = {}
 	dynawa.app_manager:after_app_start("dynawa.dyno", function(dyno)
 		dyno.events:register_for_events(self, function(ev)
 			return (ev.data and ev.data.command == "HTTP_response")
 		end)
 	end)
-	self:handle_event_timed_event()
+--	self:handle_event_timed_event()
 end
 
-function app:send_request(request)
+function app:make_request(request)
+	assert(request.server, "No server specified in HTTP request")
+	request.id = dynawa.unique_id()
+	request.port = request.port or 80
+	request.path = request.path or "/"
 	local dyno = dynawa.app_manager:app_by_id("dynawa.dyno")
 	if not dyno then
 		dynawa.popup:error("Dyno is not running, HTTP Request Service cannot control the phone")
 		return false,"No Dyno"
 	end
 	local data = {command = "HTTP_request"}
-	data.message = "GET / HTTP/1.0\r\n\r\n"
-	data.address = "www.kompost.cz"
-	data.port = 80
-	data.id = dynawa.unique_id()
+	data.message = "GET "..request.path.." HTTP/1.0\r\n\r\n"
+	data.address = assert(request.server)
+	data.port = assert(request.port)
+	data.id = request.id
 	local id,act
 	for i,a in pairs(dyno.activities) do
 		if a.status == "connected" then
@@ -29,10 +34,12 @@ function app:send_request(request)
 		end
 	end
 	if not act then
-		log("HTTP Requester: Dyno doesn't have any activity whose status is 'connected'. Trying again in a while.")
+		log("HTTP Requester: Dyno doesn't have any activity whose status is 'connected'.") --#todo
 		return
 	end 
-	log("**** Sending http request")
+	log("**** Sending http request "..request.id)
+	request.bdaddr = act.bdaddr
+	self.requests[request.id] = request
 	local stat,err = dyno:bdaddr_send_data(act.bdaddr,data)
 	if not stat then
 		log("Cannot send http request: "..err)
@@ -68,10 +75,10 @@ function app:handle_event_dyno_data_from_phone(ev)
 	self:handle_response(assert(ev.data))
 end
 
-function app:handle_event_timed_event(ev)
+--[[function app:handle_event_timed_event(ev)
 	self:send_request()
 	dynawa.devices.timers:timed_event{delay = 30000, receiver = self}
-end
+end]]
 
 function app:handle_response(response)
 	--log("HTTP response: "..dynawa.file.serialize(response))
